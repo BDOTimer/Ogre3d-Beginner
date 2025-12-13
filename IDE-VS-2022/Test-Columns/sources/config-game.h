1f6288212214531114982b096516f1da719d3c5c
BDOTimer <41667659+BDOTimer@users.noreply.github.com>
2025-12-13 14:49:54 +0300

    Update config-game.h

--- IDE-VS-2022/Test-Columns/sources/config-game.h
+++ IDE-VS-2022/Test-Columns/sources/config-game.h
@@ -5,7 +5,24 @@
 #define CONFIG_GAME_H
 #include "box/debug.h"
 
+struct  ConfigGame
+{       ConfigGame()
+        {   
+        }
 
+    const unsigned W{ 6}; /// Ширина  корзины(Well).
+    const unsigned H{15}; /// Выстота корзины(Well).
+    const unsigned N{15}; /// Количество (Gems) в фигуре(Column).
+
+    const float sizeCell  {20};
+    const float gemsRadius{20};
+
+
+        
+    static ConfigGame& get()
+    {   static ConfigGame cfg; return cfg;
+    }
+};
 
 #endif // CONFIG_GAME_H
 
